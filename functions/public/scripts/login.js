3cb01041a9ebdeba8b622ca6f8b7146adb9b4609
Power <powermukisa@gmail.com>
2018-07-14 16:52:22 +0300

    Add await function for pulling data from db

--- functions/public/scripts/login.js
+++ functions/public/scripts/login.js
@@ -6,51 +6,99 @@ function Login() {
   this.initFirebase();
 }
 
-Login.prototype.initFirebase = function() {
+var university = "";
+var userProfileIsSet = false;
+
+Login.prototype.initFirebase = function () {
   this.auth = firebase.auth();
 };
 
-Login.prototype.initFirebase = function() {
+Login.prototype.initFirebase = function () {
   this.auth = firebase.auth();
   this.database = firebase.database();
 };
 
-Login.prototype.signIn = function() {
+var user;
+
+Login.prototype.signIn = function () {
   var self = this;
   var provider = new firebase.auth.FacebookAuthProvider();
-  this.auth.signInWithPopup(provider).then(function(result) {
+  this.auth.signInWithPopup(provider).then(function (result) {
     var token = result.credential.accessToken;
-    var user = result.user;
-    //if first time user, create new user node uder users node in firebase db
-    if(!self.userExists()){
-        self.addUserToUsersDb(user);
-        // self.launchHomeScreen();
+    user = result.user;
+    if (!self.userExistsInDb()) {
+      self.addUserToUsersDb(user);
+      self.checkForProfile();
     } else {
-      self.launchHomeScreen();
+      self.checkForProfile();
     }
-  }).catch(function(error) {
+
+  }).catch(function (error) {
     var errorMessage = error.message;
-    console.log("error logging in: "+errorMessage);
+    console.log("error logging in: " + errorMessage);
   });
 };
 
-Login.prototype.userExists = function() {
-  return false;
+Login.prototype.userExistsInDb = function () {
+  return true;
 };
 
-Login.prototype.launchHomeScreen = function() {
+Login.prototype.getUniversity = function () {
+  var self = this;
+  return new Promise(function(resolve, reject) {
+    self.getUniversityFromDb();  
+    resolve("get Univeristy db Promise completed successfully");
+  });
+}
+
+Login.prototype.checkForProfile = async function () {
+  await this.getUniversityFromDb();
+  this.setUserProfileBoolean();
+  if (userProfileIsSet) {
+    this.launchHomeScreen();
+  } else {
+    this.launchProfileScreen();
+  }
+}
+
+Login.prototype.getUniversityFromDb = function () { 
+  console.log("getUniversityFromDb");
+  return firebase.database().ref('/users/' + user.uid).once('value').then(function (snapshot) {
+  university = (snapshot.val() && snapshot.val().university) || 'NOT_SET';
+  console.log("1");
+    console.log(university);
+  });
+};
+
+Login.prototype.setUserProfileBoolean = function () { 
+  console.log("setUserProfileBoolean");
+  console.log("2");
+  if (university != "NOT_SET") {
+    userProfileIsSet = true;
+  }
+console.log("User profile set? " + userProfileIsSet);
+};
+
+Login.prototype.launchHomeScreen = function () {
   window.location.href = "/";
 };
 
-Login.prototype.addUserToUsersDb = function(user) {
+Login.prototype.launchProfileScreen = function () {
+  window.location.href = "/profile";
+};
+
+Login.prototype.addUserToUsersDb = function (user) {
   return this.database.ref('users/' + user.uid).set({
     displayName: user.displayName,
-    profilePicUrl: user.photoURL
-  }).catch(function(error) {
+    profilePicUrl: user.photoURL,
+    year: "NOT_SET",
+    course: "NOT_SET",
+    university: "NOT_SET"
+  }).catch(function (error) {
     console.error('Error adding new user to Firebase Database', error);
   });
 };
 
-window.addEventListener('load' , function() {
+window.addEventListener('load', function () {
   window.Login = new Login();
 });
--- functions/public/scripts/profile.js
+++ functions/public/scripts/profile.js
@@ -15,7 +15,7 @@ function Profile() {
 }
 
 // Sets up shortcuts to Firebase features and initiate firebase auth.
-FriendlyChat.prototype.initFirebase = function() {
+Profile.prototype.initFirebase = function() {
   // Shortcuts to Firebase SDK features.
   this.auth = firebase.auth();
   this.database = firebase.database();
@@ -24,7 +24,7 @@ FriendlyChat.prototype.initFirebase = function() {
 };
 
 // Saves a new message on the Firebase DB.
-FriendlyChat.prototype.saveProfile = function(messageText) { //todo : continue from here
+Profile.prototype.saveProfile = function(messageText) { //todo : continue from here
   // Add a new message entry to the Firebase Database.
   return this.database.ref('/messages/').push({
     name: this.getUserName(),
@@ -36,7 +36,7 @@ FriendlyChat.prototype.saveProfile = function(messageText) { //todo : continue f
 };
 
 // Triggered when the send new message form is submitted.
-FriendlyChat.prototype.onProfileFormSubmit = function(e) {
+Profile.prototype.onProfileFormSubmit = function(e) {
   e.preventDefault();
   if (this.checkSignedIn()) {
     this.saveProfile("dummy text").then(function() {
@@ -45,7 +45,7 @@ FriendlyChat.prototype.onProfileFormSubmit = function(e) {
 };
 
 // Returns true if user is signed-in. Otherwise false and displays a message.
-FriendlyChat.prototype.checkSignedIn = function() {
+Profile.prototype.checkSignedIn = function() {
   // Return true if the user is signed in Firebase
   if (this.isUserSignedIn()) {
     return true;
@@ -61,5 +61,5 @@ FriendlyChat.prototype.checkSignedIn = function() {
 };
 
 window.addEventListener('load' , function() {
-  window.friendlyChat = new FriendlyChat();
+  window.friendlyChat = new Profile();
 });
