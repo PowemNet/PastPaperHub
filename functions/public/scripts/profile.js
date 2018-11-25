30e4742b559fd92dbd8c76eacadd2549ea534a47
Derek Leiro <derekleiro@gmail.com>
2018-11-25 16:28:20 +0300

    #70 - Pull request changes

--- functions/public/scripts/profile.js
+++ functions/public/scripts/profile.js
@@ -89,7 +89,8 @@ Profile.prototype.saveProfile = function (year, course, university) {
         message: 'Profile saved!',
         timeout: 2000
       };
-      document.getElementById("must-signin-snackbar").innerHTML = data;
+      //self.signInSnackbar.MaterialSnackbar.showSnackbar(data);
+      window.history.back();
     }
   });
 };
